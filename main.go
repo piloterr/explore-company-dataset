39c196857c6a000500f94fdf730ff94a52925048
Hugo Lageneste <hugo@olivia-ai.org>
2020-03-27 17:43:17 +0100

    Create sample.json and GetURLContent() method in main.go

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func main() {
-	
+	url := flag.String("url", "", "The url of the dataset.")
+	flag.Parse()
+
+	if *url == "" {
+		panic("The -url flag must be present.")
+	}
+
+	data := GetURLContent(*url)
+
+	fmt.Println(data)
+}
+
+// GetURLContent returns the content of a file in the URL
+func GetURLContent(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
 }
